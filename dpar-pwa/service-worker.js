ae626273f536d8e68d4225c24735855014c179ed
oprmspro9-cpu <oprmspro9@gmail.com>
2025-09-06 21:40:51 +0800

    Update service-worker.js

--- dpar-pwa/service-worker.js
+++ dpar-pwa/service-worker.js
@@ -1,17 +1,39 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("dpar-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ]);
+const CACHE_NAME = "dpar-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache or network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+// Activate & clean old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
   );
 });
